bb28adae7cd4edbc01f21e1babc452486cee87ee
bhavik patel <bhavikpatel576@gmail.com>
2018-12-15 18:05:04 -0600

    update padding on various areas, center play button,force favicon to update

--- client/src/components/winner.js
+++ client/src/components/winner.js
@@ -6,7 +6,7 @@ const Winner = (props) => {
     const lost = props.gameResult
     const caption = (lost ? `Sorry but you lost. The correct word is ${props.correctWord}` : "You won! You should play again")
     return (
-        <div>
+        <div className="winner">
             <h2>Game Result:</h2>
             <h2>{caption}</h2>
             <button onClick={props.onClick}>Play Again?</button>
